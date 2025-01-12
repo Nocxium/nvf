b7198835e9598b523e3945c5c4c707cfaa0cc6e7
mint <ozz.gaming@proton.me>
2025-01-12 20:30:13 +0100

    test

--- flake.nix
+++ flake.nix
@@ -3,20 +3,18 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     nvf.url = "github:notashelf/nvf";
   };
-  outputs = {
-    self,
-    nixpkgs,
-    ...
-  } @ inputs:
+  outputs = { self, nixpkgs, ... } @ inputs:
     let
-      # Only include x86_64-linux since nvf doesn't support aarch64
+      # Define supported architectures
       supportedSystems = [ "x86_64-linux" ];
+      allSystems = [ "x86_64-linux" "aarch64-linux" ];
 
       # Helper function to generate attributes for each system
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      forAllArchs = nixpkgs.lib.genAttrs allSystems;
 
       # Function to get pkgs for a specific system
-      pkgsFor = system: nixpkgs.legacyPackages.${system};
+      pkgsFor = system: import nixpkgs { system = system; };
 
       # Neovim configuration function
       mkNeovimConfig = system: { 
@@ -25,31 +23,26 @@
         isWeb ? false,
         isPython ? false,
         isLua ? false 
-      }:
-        inputs.nvf.lib.neovimConfiguration {
-          pkgs = pkgsFor system;
-          modules = [
-            ./config
-            {
-              config.vim = {
-                languages = {
-                  bash.enable = isBash;
-                  css.enable = isWeb;
-                  html.enable = isWeb;
-                  java.enable = isJava;
-                  lua.enable = isLua;
-                  python.enable = isPython;
-                };
-              };
-            }
-          ];
-        };
+      }: inputs.nvf.lib.neovimConfiguration {
+        pkgs = pkgsFor system;
+        modules = [
+          ./config
+          {
+            config.vim.languages = {
+              bash.enable = isBash;
+              css.enable = isWeb;
+              html.enable = isWeb;
+              java.enable = isJava;
+              lua.enable = isLua;
+              python.enable = isPython;
+            };
+          }
+        ];
+      };
 
       # Function to create a basic Neovim configuration for unsupported systems
       mkBasicNeovim = system:
-        let
-          pkgs = pkgsFor system;
-        in
+        let pkgs = pkgsFor system; in
         pkgs.neovim.override {
           configure = {
             customRC = ''
@@ -65,27 +58,26 @@
           };
         };
     in {
-      # Packages for supported systems (x86_64-linux)
-      packages = forAllSystems (system: {
-        default = (mkNeovimConfig system {}).neovim;
-        bash = (mkNeovimConfig system { isBash = true; }).neovim;
-        java = (mkNeovimConfig system { isJava = true; }).neovim;
-        web = (mkNeovimConfig system { isWeb = true; }).neovim;
-        python = (mkNeovimConfig system { isPython = true; }).neovim;
-        lua = (mkNeovimConfig system { isLua = true; }).neovim;
-        full = (mkNeovimConfig system {
-          isBash = true;
-          isJava = true;
-          isWeb = true;
-          isPython = true;
-          isLua = true;
-        }).neovim;
-      }) // {
-        # Basic Neovim configuration for aarch64-linux
-        "aarch64-linux" = {
-          default = mkBasicNeovim "aarch64-linux";  # This allows nix run github:xxxx/nvf#rPi
-          rPi = mkBasicNeovim "aarch64-linux";
-        };
-      };
+      # Packages for all architectures
+      packages = forAllArchs (system:
+        if system == "x86_64-linux" then {
+          default = (mkNeovimConfig system {}).neovim;
+          bash = (mkNeovimConfig system { isBash = true; }).neovim;
+          java = (mkNeovimConfig system { isJava = true; }).neovim;
+          web = (mkNeovimConfig system { isWeb = true; }).neovim;
+          python = (mkNeovimConfig system { isPython = true; }).neovim;
+          lua = (mkNeovimConfig system { isLua = true; }).neovim;
+          full = (mkNeovimConfig system {
+            isBash = true;
+            isJava = true;
+            isWeb = true;
+            isPython = true;
+            isLua = true;
+          }).neovim;
+        } else {
+          default = mkBasicNeovim system;  # Default package for `nix run`
+          rPi = mkBasicNeovim system;      # Explicit rPi package
+        }
+      );
     };
 }
